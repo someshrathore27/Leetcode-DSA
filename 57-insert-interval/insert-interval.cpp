8921062329454abf00dcc14a5e3645930c0e0194
Somesh Rathore <someshrathore27@gmail.com>
2025-07-14 17:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (44.89%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        vector<vector<int>>ans;
+        // if(n==0)
+        // {
+        //     return ans;
+        // }
+        int i=0;
+        //left
+        while(i<n && intervals[i][1]<newInterval[0])
+        {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+
+        // centre
+        while(i<n && intervals[i][0]<=newInterval[1])
+        {
+            newInterval[0]=min(newInterval[0],intervals[i][0]);
+            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        ans.push_back(newInterval);
+
+        //right
+        while(i<n)
+        {
+            ans.push_back(intervals[i]);
+            i++;
+        }
+        return ans;
+    }
+};
