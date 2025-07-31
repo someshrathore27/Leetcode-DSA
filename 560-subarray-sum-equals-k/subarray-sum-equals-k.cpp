2040aba44e9d26a313fac5222a3ab7cd0c667719
Somesh Rathore <someshrathore27@gmail.com>
2025-07-31 20:09:02 +0530

    Time: 31 ms (96.90%) | Memory: 45.5 MB (43.98%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    int subarraySum(std::vector<int>& nums, int k) {
+        unordered_map<int, int> prefix_sum_counts;
+        prefix_sum_counts[0] = 1; 
+        int current_sum = 0;
+        int count = 0;
+        
+        for (int num : nums) 
+        {
+            current_sum += num;
+            if (prefix_sum_counts.find(current_sum - k) != prefix_sum_counts.end()) {
+                count += prefix_sum_counts[current_sum - k];
+            }
+            prefix_sum_counts[current_sum]++;
+        }
+        
+        return count;
+    }
+};
