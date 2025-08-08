2e562354f96c6b46c3b1243b5e3eeaec846a18eb
Somesh Rathore <someshrathore27@gmail.com>
2025-08-08 21:33:32 +0530

    Time: 1 ms (46.77%) | Memory: 77 MB (73.31%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
@@ -16,40 +6,21 @@ public:
         ListNode* temp1=l1;
         ListNode* temp2=l2;
         int carry=0;
-        while(temp1!=NULL && temp2!=NULL)
+        while(temp1!=NULL || temp2!=NULL)
         {
-            int sum=temp1->val+temp2->val+carry;
+            int val1=(temp1!=NULL?temp1->val:0);
+            int val2=(temp2!=NULL?temp2->val:0);
+            int sum=val1+val2+carry;
             carry=sum/10;
-            int n=sum%10;
-            ListNode*temp=new ListNode(n);
+            ListNode* temp=new ListNode(sum%10);
             t->next=temp;
             t=t->next;
-            temp1=temp1->next;
-            temp2=temp2->next;
-        }
-        while(temp1!=NULL)
-        {
-            int sum=temp1->val+carry;
-            carry=sum/10;
-            int n=sum%10;
-             ListNode*temp=new ListNode(n);
-            t->next=temp;
-            t=t->next;
-            temp1=temp1->next;
-        }
-        while(temp2!=NULL)
-        {
-            int sum=temp2->val+carry;
-            carry=sum/10;
-            int n=sum%10;
-            ListNode*temp=new ListNode(n);
-            t->next=temp;
-            t=t->next;
-            temp2=temp2->next;
+            if(temp1!=NULL) temp1=temp1->next;
+            if(temp2!=NULL) temp2=temp2->next;
         }
         if(carry>0)
         {
-            ListNode*temp=new ListNode(carry);
+            ListNode* temp=new ListNode(carry);
             t->next=temp;
             t=t->next;
         }
