a3f2a74899ad7c8cb6e273a8662353c09fbd5ea1
Somesh Rathore <someshrathore27@gmail.com>
2025-09-22 15:14:31 +0530

    Time: 3 ms (89.47%) | Memory: 23.8 MB (57.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    static bool compare(const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& intervals) 
-    {
-        if(intervals.empty()){
-            return {};
-        }
-        sort(intervals.begin(), intervals.end(), compare);
-        vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); ++i) 
+    vector<vector<int>> merge(vector<vector<int>>& arr) {
+        sort(arr.begin(),arr.end());
+        vector<vector<int>>ans;
+        int idx=0;
+        int i=1;
+        ans.push_back(arr[0]);
+        while(i<arr.size())
         {
-            if(ans.back()[1] >= intervals[i][0]) 
+            if(arr[i][0]<=ans[idx][1])
             {
-                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+                ans[idx][0]=min(ans[idx][0],arr[i][0]);
+                ans[idx][1]=max(ans[idx][1],arr[i][1]);
             }
-            else 
-            {
-                ans.push_back(intervals[i]);
+            else{
+                ans.push_back(arr[i]);
+                idx++;
             }
+            i++;
         }
         return ans;
     }
