33c89cb23fd6eb78c46635472597d59a49ed9290
Somesh Rathore <someshrathore27@gmail.com>
2026-01-28 07:56:45 +0530

    Time: 648 ms (18.60%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -2,9 +2,9 @@ SELECT
     LEFT(trans_date, 7) AS month,
     country, 
     COUNT(id) AS trans_count,
-    SUM(case when state = 'approved' then 1 else 0 end) AS approved_count,
+    SUM(if(state = 'approved',1,0)) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM((case when state = 'approved' then 1 else 0 end) * amount) AS approved_total_amount
+    SUM((if(state = 'approved',1,0)) * amount) AS approved_total_amount
 FROM 
     Transactions
 GROUP BY month, country;
