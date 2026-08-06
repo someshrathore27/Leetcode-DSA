b9bbea5cf430be0c3e40b2a31ac2b606a517874e
Somesh Rathore <someshrathore27@gmail.com>
2026-08-06 18:00:19 +0530

    Time: 378 ms (43.11%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,10 @@
-# Write your MySQL query statement below
-with m1 as(
-    select managerId , count(managerId) as cnt from Employee group by(managerId)
+-- # Write your MySQL query statement below
+-- with m1 as(
+--     select managerId , count(managerId) as cnt from Employee group by(managerId)
+-- )
+-- select name from Employee inner join m1 on Employee.id=m1.managerId where m1.cnt>=5;
+
+with t1 as (
+    select managerId , count(managerId) as cnt from Employee group by managerId
 )
-select name from Employee inner join m1 on Employee.id=m1.managerId where m1.cnt>=5;
+select name from Employee e inner join t1 on e.id=t1.managerId and t1.cnt>=5;
