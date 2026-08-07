a7e26368fe114636e25ba4236f7cfc619dfa955f
Somesh Rathore <someshrathore27@gmail.com>
2026-08-07 18:50:35 +0530

    Time: 639 ms (19.13%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,3 +1,6 @@
-# Write your MySQL query statement below
-select p.project_id as project_id,CAST(AVG(e.experience_years) AS DECIMAL(10,2)) as average_years from Project p inner join Employee e on
-p.employee_id = e.employee_id group by p.project_id;
+-- # Write your MySQL query statement below
+-- select p.project_id as project_id,CAST(AVG(e.experience_years) AS DECIMAL(10,2)) as average_years from Project p inner join Employee e on
+-- p.employee_id = e.employee_id group by p.project_id;
+
+select p.project_id , round(avg(e.experience_years),2) as average_years from Project p left join Employee e 
+on p.employee_id=e.employee_id group by p.project_id;
