946843ef2c421e9d18be8376b6fa899a17ce043f
Somesh Rathore <someshrathore27@gmail.com>
2026-09-18 13:25:32 +0530

    Time: 679 ms (30.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,14 @@
+SELECT employee_id, department_id
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY employee_id
+               ORDER BY 
+                   CASE 
+                       WHEN primary_flag = 'Y' THEN 1
+                       ELSE 2
+                   END
+           ) AS rn
+    FROM Employee
+) t
+WHERE rn = 1;
