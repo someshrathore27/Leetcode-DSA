5db7ecaaa22d3d804cdd83b751704c7029c1ebe0
Somesh Rathore <someshrathore27@gmail.com>
2026-09-21 13:58:28 +0530

    Time: 346 ms (84.02%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,5 +1,2 @@
-with t1 as (
-    select distinct employee_id from Employees
-)
-select employee_id from Employees where manager_id not in (select employee_id from t1) and 
+select employee_id from Employees where manager_id not in (select employee_id from Employees) and 
 salary<30000 order by employee_id;
